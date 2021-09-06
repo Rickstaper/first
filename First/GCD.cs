daea144ed5805972b3767c9df248f10d46398d9c
Rickstaper <karchevskji@gmail.com>
2021-09-06 09:22:59 +0300

    Add method GetGcdByEuclidean with different values

---
+++ First/GCD.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    class GCD
+    {
+        public static int GetGcdByEuclidean(int a, int b)
+        {
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException (nameof (GetGcdByEuclidean));
+            }
+
+            if (a == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException (nameof(a));
+            }
+
+            if (b == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException (nameof (b));
+            }
+
+            if (a < 0)
+            {
+                a *= -1;
+            }
+
+            if (b < 0)
+            {
+                b *= -1;
+            }
+
+            while(a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a + b;
+        }
+
+        public static int GetGcdByEuclidean(int a, int b, int c)
+        {
+            if (a == 0 && b == 0 && c == 0)
+            {
+                throw new ArgumentException (nameof (GetGcdByEuclidean));
+            }
+
+            if (a < int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException (nameof (a));
+            }
+
+            if (b < int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException (nameof (a));
+            }
+
+            if (c < int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException (nameof (a));
+            }
+
+            if (a < 0)
+            {
+                a *= -1;
+            }
+
+            if (b < 0)
+            {
+                b *= -1;
+            }
+
+            if (c < 0)
+            {
+                c *= -1;
+            }
+
+            while(a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            int result = a + b;
+
+            while(result != 0 && c != 0)
+            {
+                if (result > c)
+                    result %= c;
+                else
+                    c %= result;
+            }
+
+            result += c;
+
+            return result;
+        }
+
+        public static int GetGcdByEuclidean(int a, int b, params int[] other)
+        {
+            if (a == 0 && b == 0 && other.All (i => i == 0))
+                throw new ArgumentException (nameof (GetGcdByEuclidean));
+
+            if (a == int.MinValue)
+                throw new ArgumentOutOfRangeException (nameof (a));
+
+            if (b == int.MinValue)
+                throw new ArgumentOutOfRangeException (nameof (b));
+
+            if (a < 0)
+                a *= -1;
+
+            if (b < 0)
+                b *= -1;
+
+            for (int i = 0; i < other.Length; i++)
+            {
+                if (other[i] == int.MinValue)
+                    throw new ArgumentOutOfRangeException (nameof (other));
+
+                if (other[i] < 0)
+                    other[i] *= -1;
+            }
+
+            while(a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            int result = a + b, otherResult = 0;
+
+            for (int i = 1; i < other.Length; i++)
+            {
+                if (other[i - 1] == 0 || other[i] == 0)
+                {
+                    continue;
+                }
+
+                if (other[i - 1] > other[i])
+                    other[i - 1] %= other[i];
+                else
+                    other[i] %= other[i - 1];
+
+                otherResult = other[i - 1] + other[i];
+            }
+
+            while(result != 0 && otherResult != 0)
+            {
+                if (result > otherResult)
+                    result %= otherResult;
+                else
+                    otherResult %= result;
+            }
+
+            if (result + otherResult == 0)
+                return 1;
+
+            return otherResult + result;
+        }
+    }
+}
